4facd58a960616746d5b4fd71b9f0651cdc782f6
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-02-08 18:42:07 -0500

    Adding the Product-Delete Functionality

--- models/product.js
+++ models/product.js
@@ -47,7 +47,12 @@ module.exports = class Product {
     });
   }
 
-  static delete() {}
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts));
+    });
+  }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
