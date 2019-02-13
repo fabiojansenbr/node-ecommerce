983fa6cc4df98efd4a6de8b3dd65259f7f6a3070
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-02-13 01:31:11 -0500

    added sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Andrew17_"
+const sequelize = new Sequelize("node-complete", "root", "Andrew17_", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
